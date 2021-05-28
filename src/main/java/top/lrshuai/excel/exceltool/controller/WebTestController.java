f7422cd3af16442fc96507615900366edb076039
lrs <1006059906@qq.com>
2021-05-28 15:21:16 +0800

    add

--- src/main/java/top/lrshuai/excel/exceltool/controller/WebTestController.java
+++ src/main/java/top/lrshuai/excel/exceltool/controller/WebTestController.java
@@ -10,7 +10,10 @@ import org.springframework.web.multipart.MultipartFile;
 import top.lrshuai.excel.exceltool.entity.ExcelDataDto;
 import top.lrshuai.excel.exceltool.listener.ExcelDataListener;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.net.URLEncoder;
+import java.util.ArrayList;
 
 @Controller
 @RequestMapping("/test")
@@ -33,4 +36,17 @@ public class WebTestController {
         return "success";
     }
 
+    /**
+     * 文件下载（失败了会返回一个有部分数据的Excel）
+     */
+    @GetMapping("download")
+    public void download(HttpServletResponse response) throws IOException {
+        // 这里注意 有同学反应使用swagger 会导致各种问题，请直接用浏览器或者用postman
+        response.setContentType("application/vnd.ms-excel");
+        response.setCharacterEncoding("utf-8");
+        // 这里URLEncoder.encode可以防止中文乱码 当然和easyexcel没有关系
+        String fileName = URLEncoder.encode("测试", "UTF-8").replaceAll("\\+", "%20");
+        response.setHeader("Content-disposition", "attachment;filename*=utf-8''" + fileName + ".xlsx");
+        EasyExcel.write(response.getOutputStream(), ExcelDataDto.class).sheet("模板").doWrite(new ArrayList());
+    }
 }
--- src/main/java/top/lrshuai/excel/exceltool/entity/UserDto.java
+++ src/main/java/top/lrshuai/excel/exceltool/entity/UserDto.java
@@ -1,15 +1,22 @@
 package top.lrshuai.excel.exceltool.entity;
 
 import com.alibaba.excel.annotation.ExcelProperty;
+import com.alibaba.excel.annotation.format.DateTimeFormat;
 import com.alibaba.excel.annotation.write.style.ColumnWidth;
 import lombok.Data;
+import lombok.experimental.Accessors;
 import top.lrshuai.excel.exceltool.converter.LocalDateTimeConverter;
 import top.lrshuai.excel.exceltool.dropdown.annotation.DropDownFields;
 
 import java.time.LocalDateTime;
+import java.util.Date;
 
+/**
+ * ColumnWidth 定义列宽
+ */
 @ColumnWidth(25)
 @Data
+@Accessors(chain = true)
 public class UserDto {
 
     @ExcelProperty("用户名称")
@@ -25,7 +32,18 @@ public class UserDto {
     @ExcelProperty("职业")
     private String occupation;
 
+    /**
+     * 自定义日期转换器 LocalDateTimeConverter
+     */
     @ColumnWidth(50)
     @ExcelProperty(value = "注册时间",converter = LocalDateTimeConverter.class)
     private LocalDateTime createTime;
+
+    /**
+     * 使用注解 @DateTimeFormat
+     */
+    @ExcelProperty(value = "发财时间")
+    @DateTimeFormat("yyyy-MM-dd HH:mm:ss")
+    @ColumnWidth(50)
+    private Date startTime;
 }
